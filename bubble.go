23d1b22f7030cbd4a8e256c6de08f0e864c65fac
m@shubi.ch <m@shubi.ch>
2023-07-16 08:48:48 +0200

    refactoring

--- bubble.go
+++ bubble.go
@@ -2,13 +2,15 @@ package gort
 
 /*
 for i = 1:n,
-    swapped = false
-    for j = n:i+1,
-        if a[j] < a[j-1],
-            swap a[j,j-1]
-            swapped = true
-    → invariant: a[1..i] in final position
-    break if not swapped
+
+	swapped = false
+	for j = n:i+1,
+	    if a[j] < a[j-1],
+	        swap a[j,j-1]
+	        swapped = true
+	→ invariant: a[1..i] in final position
+	break if not swapped
+
 end
 
 PROPERTIES
@@ -18,18 +20,17 @@ O(n2) comparisons and swaps
 Adaptive: O(n) when nearly sorted
 */
 func Bubble[C Ordered](arr []C) {
-	var size int = len(arr)
-	var currIndex int
-	var swap C
-	var isSwapped bool = false
+	var (
+		size      int = len(arr)
+		currIndex int
+		isSwapped bool = false
+	)
 
 	for currIndex = 0; currIndex < size-1; currIndex++ {
 		isSwapped = false
 		for i := 0; i < size-currIndex-1; i++ {
 			if arr[i] > arr[i+1] { //this moves the biggest el to the end of the list
-				swap = arr[i]
-				arr[i] = arr[i+1]
-				arr[i+1] = swap
+				arr[i], arr[i+1] = arr[i+1], arr[i]
 				isSwapped = true
 			}
 		}
